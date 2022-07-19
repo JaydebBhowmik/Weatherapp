6c3c4075067d07f72d8e16d89c9706f513ac0454
Jaydeb <jaydebbhowmick86@gmail.com>
2022-07-19 15:47:13 +0530

    resolve console issues

--- src/component/SearchForm.js
+++ src/component/SearchForm.js
@@ -12,37 +12,54 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function SearchForm() {
-    const [data, setData] = useState("");
-    const [inputCity, setInputCity] = useState("");
+    // const [data, setData] = useState();
+    const [inputCity, setInputCity] = useState();
+    const [currentdate, setCurrentdate] = useState('')
+    const [name, setName] = useState('')
+    const [icon, setIcon] = useState('')
+    const [temp, setTemp] = useState('')
+    const [precipitation, setPrecipitation] = useState('')
+    const [humidity, setHumidity] = useState('')
+    const [windSpeed, setWindSpeed] = useState('')
+    const [windDir, setwindDir] = useState('')
+    const [forecastday, setForecastday] = useState([])
+    
 
     const getWeatherDetails = (cityName) => {
         if(!cityName) return
         const apiUrl = "http://api.weatherapi.com/v1/forecast.json?key="+ KEY +"&q=" + cityName +"&days=10&aqi=yes&alerts=yes"
         axios.get(apiUrl).then((res) => {
             console.log("response", res.data)
-            setData(res.data)
+            // setData(res.data)
+            setCurrentdate(res.data.current.last_updated)
+            setName(res.data.location.name)
+            setIcon(res.data.current.condition.icon)
+            setTemp(res.data.current.temp_c)
+            setPrecipitation(res.data.current.precip_in)
+            setHumidity(res.data.current.humidity)
+            setWindSpeed(res.data.current.wind_kph)
+            setwindDir(res.data.current.wind_dir)
+            setForecastday(res.data.forecast.forecastday)
         }).catch((err) => {
             console.log("Error", err)
         })
     }
+
     const handleChangeInput = (e) => {
         setInputCity(e.target.value)
-        // console.log("value", e.target.value) 
     }
     const handleSearch = () => {
-        getWeatherDetails(inputCity)
+        getWeatherDetails(inputCity, currentdate)
+
     }
 
     useEffect(()=> {
         getWeatherDetails("Kolkata")
     },[])
-    
-    // const forecast = data.forecast;
-    // console.log(forecast.forecastday);
 
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentdate = data.current.last_updated
-    const formatDate = Moment(currentdate).format("MMM Do");
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    // const currentdate = data.current.last_updated
+    const formatDate = Moment(currentdate).format("MMM Do")
 
     return (
         <> 
@@ -67,20 +84,20 @@ function SearchForm() {
                 
                 <Row className='mb-5'>
                     <Col className='mt-5'>
-                        <h2 className='current-city'>{data.location.name}</h2>
+                        <h2 className='current-city'>{name}</h2>
                         <h4 className='current-date'>{days[new Date(currentdate).getDay()]}, {formatDate}</h4>
 
                         <h4>overcast</h4>
                         <h4 className='ctemp'>
-                            <span className='ctemp-icon'><img alt='icon' src={data.current.condition.icon}/> </span>
-                            <strong className='ctemp-unit'>{data.current.temp_c} °C</strong>
+                            <span className='ctemp-icon'><img alt='icon' src={icon}/> </span>
+                            <strong className='ctemp-unit'>{temp} °C</strong>
                         </h4>
                     </Col>
 
                     <Col className='mt-5 col-3 left-box'>
-                        <h4>Precipitation : {data.current.precip_in} %</h4>
-                        <h4>Humidity : {data.current.humidity} %</h4>
-                        <h4>Wind : {data.current.wind_kph} khp {data.current.wind_dir}</h4>
+                        <h4>Precipitation : {precipitation} %</h4>
+                        <h4>Humidity : {humidity} %</h4>
+                        <h4>Wind : {windSpeed} khp {windDir}</h4>
                     </Col>
 
                 </Row>
@@ -88,7 +105,7 @@ function SearchForm() {
                 <hr />
                 <Row>
                     {
-                        data.forecast.forecastday.map((items, index) =>
+                        forecastday.map((items, index) =>
                         <Col className='mt-5'>
                             <div key={index}>
                                 <h5> <strong>{days[new Date(items.date).getDay()]}</strong> </h5>
@@ -97,7 +114,6 @@ function SearchForm() {
                                 <p className='mb-0'> Humidity {items.day.avghumidity}</p>
                                 <p className='mb-0'>{items.day.condition.text}</p>
                             </div>
-                    
                         </Col>
                         )
                     }
